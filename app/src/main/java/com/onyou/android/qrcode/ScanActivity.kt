0c69aac2c540d03cc51a32a9a29124658fb9b567
onyou.moon <onyou0031@naver.com>
2020-07-13 17:09:26 +0900

    가로 모드

--- app/src/main/java/com/onyou/android/qrcode/ScanActivity.kt
+++ app/src/main/java/com/onyou/android/qrcode/ScanActivity.kt
@@ -11,8 +11,10 @@ class ScanActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-
-        IntentIntegrator(this).initiateScan()
+        val qrScan = IntentIntegrator(this)
+        qrScan?.setOrientationLocked(false)
+        qrScan?.setPrompt("으아아아아 테스트다아아")
+        qrScan?.initiateScan()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
