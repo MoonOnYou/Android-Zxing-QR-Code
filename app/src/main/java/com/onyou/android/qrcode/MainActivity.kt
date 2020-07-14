be6b375978f1aaed432158f2a7e0710c58403ecf
onyou.moon <onyou0031@naver.com>
2020-07-14 13:23:53 +0900

    QRcode 바깥부분 커스텀 scan 액티비티 시작할때 startActivity 가 아닌 IntentIntegrator(this)   .setOrientationLocked(false)                        // 세로모드 ( 매니페스트도 같이 설정)     .setPrompt("테스트으으으")                           //스캐너 안에 있는 텍스트 설정       .setCaptureActivity(ScanActivity::class.java)   // 다음 액티비티 적어줌         .initiateScan()                               // 로 시작
    그리고 시작한 액티비티에서 onActivityResult 를 오버라딩 해준다 .
     // val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
     로 result 받아 올수 있음

--- app/src/main/java/com/onyou/android/qrcode/MainActivity.kt
+++ app/src/main/java/com/onyou/android/qrcode/MainActivity.kt
@@ -2,7 +2,9 @@ package com.onyou.android.qrcode
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         textViewGoToScan.setOnClickListener {
-            startActivity(Intent(this, ScanActivity::class.java))
+            IntentIntegrator(this).setOrientationLocked(false).setPrompt("테스트으으으")
+                .setCaptureActivity(ScanActivity::class.java).initiateScan()
+        }
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        val toast : String?
+        if (result != null) {
+            toast = if (result.contents == null) {
+                "Cancelled from fragment"
+            } else {
+                "Scanned from fragment: " + result.contents
+            }
+            Toast.makeText(this, toast, Toast.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/com/onyou/android/qrcode/ScanActivity.kt
+++ app/src/main/java/com/onyou/android/qrcode/ScanActivity.kt
@@ -1,34 +1,48 @@
 package com.onyou.android.qrcode
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.google.zxing.integration.android.IntentIntegrator
+import androidx.appcompat.app.AppCompatActivity
+import com.journeyapps.barcodescanner.CaptureManager
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import kotlinx.android.synthetic.main.activity_scan.*
 
-class ScanActivity : AppCompatActivity() {
+class ScanActivity : AppCompatActivity(), DecoratedBarcodeView.TorchListener {
+    private var manager : CaptureManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
-        val qrScan = IntentIntegrator(this)
-        qrScan?.setOrientationLocked(false)
-        qrScan?.setPrompt("으아아아아 테스트다아아")
-        qrScan?.initiateScan()
+
+        manager = CaptureManager(this, barcodeView)
+        manager!!.initializeFromIntent(intent, savedInstanceState)
+        manager!!.decode()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
-        if (result != null) {
-            if (result.contents == null) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
-                // todo
-            } else {
-                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
-                // todo
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
+    override fun onTorchOn() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onTorchOff() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onResume() {
+        super.onResume()
+        manager?.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        manager?.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        manager?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        manager?.onSaveInstanceState(outState)
     }
 }
